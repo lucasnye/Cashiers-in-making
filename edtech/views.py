1bd3da598586dfe6696f4039853ef4d3e2eb1253
sammsze <samiesze@gmail.com>
2025-06-18 02:21:12 +0800

    Update views.py

--- edtech/views.py
+++ edtech/views.py
@@ -4,3 +4,5 @@ def index(request):
     return render(request, "edtech/index.html")
 def real_time_transcription(request):
     return render(request, "edtech/rtt.html")
+def dyslexia_format(request):
+    return render(request, "edtech/dyslexia_upload")
