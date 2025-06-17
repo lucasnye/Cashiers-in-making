2ca9165dde4725297608b7a121a3f0cb6a80a7e9
sammsze <samiesze@gmail.com>
2025-06-18 02:18:49 +0800

    Update routing.py
    for dyslexia

--- edtech/routing.py
+++ edtech/routing.py
@@ -3,4 +3,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'^ws/live_stt/$', consumers.LiveSTTConsumer.as_asgi()),
+    re_path(r'^ws/dyslexia_format/$', consumers.DyslexiaPDFWebSocketConsumer.as_asgi()),
 ]
